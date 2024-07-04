4279b4bcf1105ab3414435950976963409d3b5de
beech <beechmanbopp@gmail.com>
2024-07-04 02:05:12 -0500

    Nvidia installer fix

--- nvidia.sh
+++ nvidia.sh
@@ -7,7 +7,7 @@ sudo pacman -S linux-headers --noconfirm
 sudo pacman -S nvidia-dkms libglvnd nvidia-utils opencl-nvidia lib32-libglvnd lib32-nvidia-utils lib32-opencl-nvidia nvidia-settings --noconfirm
 
 sed -i 's/^MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-sed -i 's/^ rw/rw nvidia-drm.modeset=1/' /boot/loader/entries/arch.conf
+sed -i 's/^ rw/ rw nvidia-drm.modeset=1/' /boot/loader/entries/arch.conf
 
 sudo mkdir /etc/pacman.d/hooks
 touch /etc/pacman.d/hooks/nvidia.hook
