42af495a112185657b96e09b9d0efcdb8658bfe6
beech <beechmanbopp@gmail.com>
2024-07-09 01:33:54 -0500

    Updated AUR setup

--- aur.sh
+++ aur.sh
@@ -10,23 +10,9 @@ cd "${HOME}"
 echo "CLONING: YAY"
 git clone "https://aur.archlinux.org/yay.git"
 
-
-PKGS=(
-
-    # Browsers ------------------------------------------------------
-
-    'floorp'                 # Floorp Browser
-
-
-)
-
 cd ${HOME}/yay
 makepkg -si
 
-for PKG in "${PKGS[@]}"; do
-    yay -S --noconfirm $PKG
-done
-
 echo
-echo "Done!"
+echo "Yay setup complete."
 echo
