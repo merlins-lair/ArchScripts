797c2ddfaf56d53f1f23211146642ac456371bc1
beech <beechmanbopp@gmail.com>
2025-01-26 03:03:41 -0600

    nvidia

--- install3.sh
+++ install3.sh
@@ -28,8 +28,19 @@ setup_aur () {
     echo "Yay installed."
 }
 
+setup_nvidia () {
+    echo "Setting up Nvidia drivers..."
+    sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings --noconfirm --needed
+    sudo sed -i 's/^MODULES=().*/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+    sudo sed -i 's/\(HOOKS=.*\) kms/\1/' /etc/mkinitcpio.conf
+    sudo mkinitcpio -P
+    sudo mkdir -p /etc/pacman.d/hooks/ && sudo mv ./nvidia.hook /etc/pacman.d/hooks/
+    sudo sh -c 'echo " nvidia-drm.modeset=1 nvidia_drm.fbdev=1 nvidia-drm.ForceCompositionPipeline=1 nvidia.NVreg_EnableGpuFirmware=0" >> /boot/loader/entries/arch.conf'
+    echo "Nvidia driver setup finished."
+}
+
 while true; do
-    options=("Install GNOME + SDDM" "Install KDE + SDDM" "Install Software" "Setup Yay" "Exit")
+    options=("Install GNOME + SDDM" "Install KDE + SDDM" "Install Software" "Setup Yay" "Setup Nvidia Drivers" "Exit")
 
     echo "Debian Server Setup: "
     select opt in "${options[@]}"; do
@@ -38,7 +49,8 @@ while true; do
             2) install_kde; break ;;
             3) install_software; break ;;
             4) setup_aur; break ;;
-            5) break 2 ;;
+            5) setup_nvidia; break ;;
+            6) break 2 ;;
             *) echo "Invalid" >&2
         esac
     done
