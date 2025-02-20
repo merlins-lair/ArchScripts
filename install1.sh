5ea9f5468461773059aab4acf9332ed6aa22c555
beech <beechmanbopp@gmail.com>
2025-02-19 19:07:52 -0600

    Rankmirrors

--- install1.sh
+++ install1.sh
@@ -66,12 +66,7 @@ pacman -Sy
 pacman -S archlinux-keyring --noconfirm 
 pacman -S pacman-contrib terminus-font --noconfirm --needed 
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-pacman -S reflector rsync --noconfirm --needed
-if ! command -v reflector &> /dev/null; then
-    echo "Reflector not found - reinstalling..."
-    pacman -S reflector --noconfirm --needed
-fi
-reflector -a 48 -c "US" -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+rankmirrors -n 10 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 # install arch
 echo "-------------------------------------------------"
