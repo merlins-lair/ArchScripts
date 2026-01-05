097ae0990ce3344a99bdb9599a3b204af2ba3583
beech <beechmanbopp@gmail.com>
2026-01-05 17:06:02 -0600

    Fix chroot

--- install1.sh
+++ install1.sh
@@ -19,26 +19,26 @@ fi
 echo -e "\nFormatting disk...\n$HR"
 
 # disk prep
-sgdisk -Z $DISK # zap all on disk
-sgdisk -a 2048 -o $DISK  # new gpt disk 2048 alignment
+sgdisk -Z $DISK 2>/dev/null # zap all on disk
+sgdisk -a 2048 -o $DISK 2>/dev/null  # new gpt disk 2048 alignment
 
 # create partitions
-sgdisk -n 1:0:1024M $DISK  # partition 1 (boot)
-sgdisk -n 2:0:8G $DISK  # partition 2 (SWAP - change to desired size)
-sgdisk -n 3:0:75G $DISK # partition 3 (root - change to desired size)
-sgdisk -n 4:0:0 $DISK # partition 4 (home, remaining space)
+sgdisk -n 1:0:1024M $DISK 2>/dev/null  # partition 1 (boot)
+sgdisk -n 2:0:8G $DISK 2>/dev/null  # partition 2 (SWAP - change to desired size)
+sgdisk -n 3:0:75G $DISK 2>/dev/null # partition 3 (root - change to desired size)
+sgdisk -n 4:0:0 $DISK 2>/dev/null # partition 4 (home, remaining space)
 
 # partition types
-sgdisk -t 1:ef00 $DISK 
-sgdisk -t 2:8200 $DISK 
-sgdisk -t 3:8300 $DISK 
-sgdisk -t 4:8300 $DISK 
+sgdisk -t 1:ef00 $DISK 2>/dev/null
+sgdisk -t 2:8200 $DISK 2>/dev/null
+sgdisk -t 3:8300 $DISK 2>/dev/null
+sgdisk -t 4:8300 $DISK 2>/dev/null
 
 # label partitions
-sgdisk -c 1:"boot" $DISK 
-sgdisk -c 2:"swap" $DISK 
-sgdisk -c 3:"root" $DISK 
-sgdisk -c 4:"home" $DISK 
+sgdisk -c 1:"boot" $DISK 2>/dev/null
+sgdisk -c 2:"swap" $DISK 2>/dev/null
+sgdisk -c 3:"root" $DISK 2>/dev/null
+sgdisk -c 4:"home" $DISK 2>/dev/null 
 
 echo -e "\nCreating Filesystems...\n$HR"
 
@@ -58,7 +58,13 @@ mkdir /mnt/home
 mount ${DISK}1 /mnt/boot
 mount ${DISK}4 /mnt/home
 
-cp install2.sh /mnt/
+echo "-------------------------------------------------"
+echo "Downloading 2nd install script"
+echo "-------------------------------------------------"
+
+curl https://git.merlinslair.net/beech/ArchScripts/raw/branch/rework/install2.sh -o /mnt/install2.sh
+sed -i 's/\r$//' /mnt/install2.sh
+chmod +x /mnt/install2.sh
 
 echo "-------------------------------------------------"
 echo "Enabling Parallel Downloads"
@@ -89,4 +95,4 @@ echo "Finished install script 1. Run install2.sh"
 echo "-------------------------------------------------"
 
 # chroot
-arch-chroot /mnt ./install2.sh
+arch-chroot /mnt /install2.sh
